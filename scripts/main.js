b87dc8ae88ac34a3251f0ada77b4fc000779632a
AZ70code <alexzubov1970@gmail.com>
2024-06-09 22:20:04 +0100

    created pop-up

--- scripts/main.js
+++ scripts/main.js
@@ -15,60 +15,83 @@
   }
 })();
 //============= form ========================
-const form = document.getElementById("form");
-const buttonSubmit = document.getElementById("submitBtn");
+const forms = document.getElementsByTagName("form");
 
 // add and remove placeholders
-form.addEventListener("focusin", (e) => {
-  if (e.target.tagName === "INPUT") {
-    e.target.placeholder = "";
-  }
-});
-form.addEventListener("focusout", (e) => {
-  if (e.target.tagName === "INPUT") {
-    e.target.placeholder = e.target.dataset.placeholder;
-  }
-});
+for(let form of forms) {
+	form.addEventListener("focusin", (e) => {
+		if (e.target.tagName === "INPUT") {
+			e.target.placeholder = "";
+		}
+	});
+	form.addEventListener("focusout", (e) => {
+		if (e.target.tagName === "INPUT") {
+			e.target.dataset.placeholder ? e.target.placeholder = e.target.dataset.placeholder : "";
+		}
+	});
+//===========tracking changes in the form fields=========
+
+	form.addEventListener('change', (e) => {
+		const form = e.target.closest('form');
+		const buttonSubmit = form.querySelector("[type='submit']");
+
+		formObserver(form, e.target, buttonSubmit)
+		form.addEventListener("submit", (e) => {
+			const body = Object.fromEntries(new FormData(e.target).entries());
+			e.preventDefault();
+			buttonSubmit.setAttribute("disabled", "true");
+			console.log(form.id)
+			if(form.id == 'form') {
+				formSubmit(body, form);
+			}
+			if(form.id == 'modal-form') {
+				closePopup();
+			}
+		});
+	})
+}
 //============= validation form ========================
 
-//===========tracking changes in the form fields=========
-form.addEventListener("change", (e) => {
-	if(e.target.tagName === "INPUT"){
-    removeErrorMessage(e.target);
-    if (e.target.id == "name" && e.target.value.trim() == "") {
-			addErrorMessage(e.target)
+function formObserver(form, target, button) {
+		if(target.tagName === "INPUT" && target.name !== 'agree'){
+			removeErrorMessage(target);
+			if (target.name == "name" && target.value.trim() == "") {
+				addErrorMessage(target)
+			}
+			if (target.name == "password" && target.value.trim() == "") {
+				addErrorMessage(target)
+			}
+			if (target.name == "email") {
+				inputValidation(
+					emailValidation,
+					target,
+					"Input valid email"
+				);
+			}
+			if (target.name == "phone") {
+				inputValidation(
+					phoneValidation,
+					target,
+					"Input phone number like +353XX XXX XXXX"
+				);
+			}
 		}
-    if (e.target.id == "email") {
-      inputValidation(
-        emailValidation,
-        e.target,
-        "Input valid email"
-      );
-    }
-    if (e.target.id == "phone") {
-      inputValidation(
-        phoneValidation,
-        e.target,
-        "Input phone number like +353XX XXX XXXX"
-      );
-    }
-  }
-  // ====check if form fields contain errors=======
-  const requireFields = document.querySelectorAll(".required");
-  let error = 0;
-  let errorMessage = null;
-  for (let i = 0; i < requireFields.length; i++) {
-    let input = requireFields[i];
-    if (input.value == "") {
-      error++;
-    }
-    errorMessage = input.parentElement.querySelector(".form-error");
-    if (errorMessage) error++;
-  }
-  if (!error) {
-    buttonSubmit.removeAttribute("disabled");
-  }
-});
+		// ====check if form fields contain errors=======
+		const requireFields = form.querySelectorAll(".required");
+		let error = 0;
+		let errorMessage = null;
+		for (let i = 0; i < requireFields.length; i++) {
+			let input = requireFields[i];
+			if (input.value == "") {
+				error++;
+			}
+			errorMessage = input.parentElement.querySelector(".form-error");
+			if (errorMessage) error++;
+		}
+		if (!error) {
+			button.removeAttribute("disabled");
+		}
+}
 // check input field values and show error messages
 function inputValidation(validator, input, message) {
   const formError = input.parentElement.querySelector(".form-error");
@@ -80,12 +103,6 @@ function inputValidation(validator, input, message) {
   }
   return false;
 }
-form.addEventListener("submit", (e) => {
-  const body = Object.fromEntries(new FormData(e.target).entries());
-  e.preventDefault();
-  formSubmit(body);
-  buttonSubmit.setAttribute("disabled", "true");
-});
 
 function addErrorMessage(
   input,
@@ -113,7 +130,7 @@ function phoneValidation(input) {
 }
 // ======Imitation of sending the form================
 
-async function formSubmit(body) {
+async function formSubmit(body, form) {
   const value = document.getElementById("name").value.trim();
   let response = await fetch("/index.html", {
     method: "POST",
@@ -243,3 +260,41 @@ calculatorForm.addEventListener("submit", (e) => {
     result.innerText = `Your Body Mass Index is ${bmi}. This is considered as obese`;
   }
 });
+// =======popup======================================
+const modal = document.querySelector(".modal");
+function openPopup() {
+	modal.classList.add("open");
+	document.body.classList.add("modal-lock");
+}
+function closePopup() {
+	const form = document.getElementById("modal-form");
+	form.reset();
+	const errors = form.querySelectorAll(".error");
+	for(let input of errors) {
+		removeErrorMessage(input)
+	}
+
+	modal.classList.remove("open");
+	document.body.classList.remove("modal-lock");
+}
+//======= toggle password icon =========================================
+const iconIn = document.getElementById('pass-in-show');
+const iconUp = document.getElementById('pass-up-show');
+if (iconIn) {
+	iconIn.addEventListener('click', showPassword)
+}
+if (iconUp) {
+	iconUp.addEventListener('click', showPassword)
+}
+function showPassword(e) {
+	e.preventDefault()
+	let target = e.target;
+	let input = target.parentNode.querySelector('input')
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view-pass');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view-pass');
+		input.setAttribute('type', 'password');
+	}
+}
